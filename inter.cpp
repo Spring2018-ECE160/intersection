19491fd161b6749f5b3638a16a82fb8e16f202ea
raymondminglee <raymondminglee@gmail.com>
2018-04-26 20:32:25 -0400

    again

--- inter.cpp
+++ inter.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-        vector<int> s{0, 1, 2, 3, 4};
+        vector<int> s{0, 1, 2, 3, 4, 5};
         vector<int> c{3, 5, 7, 9, 10, 22, 33};
 
         for (int i = 0; i < c.size(); i++)
@@ -14,7 +14,7 @@ int main()
         auto check = find(begin(s), end(s), c[i]);
         if (check != end(s)) {
                 cout << "intersect element "<< c[i] << endl;
-                break;
+                
                 }
         }
 }
