219186145b0b04e3e90b327fbc5a407f52c94f13
raymondminglee <raymondminglee@gmail.com>
2018-04-26 20:24:39 -0400

    inter

---
+++ inter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+        vector<int> s{0, 1, 2, 3, 4};
+        vector<int> c{3, 5, 7, 9, 10, 22, 33};
+
+        for (int i = 0; i < c.size(); i++)
+        {
+        auto check = find(begin(s), end(s), c[i]);
+        if (check != end(s)) {
+                cout << "intersect element "<< c[i] << endl;
+                break;
+                }
+        }
+}
